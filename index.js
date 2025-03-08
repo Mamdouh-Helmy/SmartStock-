75abd932293b694d429adf28a0c5614505ef8a43
Mamdouh-Helmy <mm9349263@gmail.com>
2025-03-08 16:09:04 +0200

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app); // استخدام http server لتشغيل Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://stockmaster-4dbcc.web.app'],
+    origin: ['http://localhost:5173', 'https://store-management-467c1.web.app'],
     methods: ['GET', 'POST'],
   },
 });
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 // إعداد CORS
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://stockmaster-4dbcc.web.app'],
+    origin: ['http://localhost:5173', 'https://store-management-467c1.web.app'],
     credentials: true,
   })
 );
